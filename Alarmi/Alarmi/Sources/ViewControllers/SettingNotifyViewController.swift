02f8ad77ad31d7a6a039eee337cac8138cadf191
wody-d <maxwodyd98@gmail.com>
2022-07-16 01:30:34 +0900

    ✨ 알림 설정 화면 생성

---
+++ Alarmi/Alarmi/Sources/ViewControllers/SettingNotifyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingNotifyViewController.swift
+//  Alarmi
+//
+//  Created by Woody on 2022/07/16.
+//  Copyright © 2022 MoTe. All rights reserved.
+//
+
+import UIKit
+
+class SettingNotifyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
