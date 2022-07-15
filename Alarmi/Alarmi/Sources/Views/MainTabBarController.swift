93c56f2371cd3ce2ff62b42443d9b4856905321d
wody-d <maxwodyd98@gmail.com>
2022-07-15 09:43:44 +0900

    ✨ MainTabBarController 파일 생성

---
+++ Alarmi/Alarmi/Sources/Views/MainTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabBarController.swift
+//  Alarmi
+//
+//  Created by Woody on 2022/07/15.
+//  Copyright © 2022 MoTe. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
