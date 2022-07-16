255fc11b9135a1140f3dba8ca741865713a6b704
Chaeho-Min <james118@naver.com>
2022-07-16 12:49:23 +0900

    ✨ SwiftUI 프리뷰 익스텐션 추가

--- Alarmi/Alarmi/Sources/Global/Extensions/UIViewController+.swift
+++ Alarmi/Alarmi/Sources/Global/Extensions/UIViewController+.swift
@@ -16,3 +16,25 @@ extension UIViewController {
         return navigationController
     }
 }
+
+#if DEBUG
+import SwiftUI
+
+extension UIViewController {
+
+    private struct Preview: UIViewControllerRepresentable {
+        let viewController: UIViewController
+
+        func makeUIViewController(context: Context) -> UIViewController {
+            return viewController
+        }
+
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        }
+    }
+
+    func toPreview() -> some View {
+        Preview(viewController: self)
+    }
+}
+#endif
