0cafbb4db5ccb60c49bc36e7f229d381317ff06f
wody-d <maxwodyd98@gmail.com>
2022-07-16 16:48:12 +0900

    ✨ 알림 세팅 화면 생성

---
+++ Alarmi/Alarmi/Sources/ViewControllers/SettingAlarm/SettingNotifyViewController.swift
@@ -0,0 +1,87 @@
+//
+//  SettingNotifyViewController.swift
+//  Alarmi
+//
+//  Created by Woody on 2022/07/16.
+//  Copyright © 2022 MoTe. All rights reserved.
+//
+
+import UIKit
+
+class SettingNotifyViewController: UIViewController {
+
+    private let descriptionLabel: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.text = "마지막으로 전화한지 7일이 되면 알림을 보내드려요."
+        $0.setDynamicFont(.body)
+        return $0
+    }(UILabel())
+
+    private let alarmSetView: AlarmSetView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(AlarmSetView())
+
+    private let alarmAgainSetView: AlarmAgainSetView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(AlarmAgainSetView())
+
+    private let alarmAgainSetDescriptionLabel: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.text = "저희가 적절한 간격으로 알림을 다시 보내드려요."
+        $0.textColor = .secondaryLabel
+        $0.textAlignment = .right
+        $0.setDynamicFont(.footnote)
+        return $0
+    }(UILabel())
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        attribute()
+        layout()
+    }
+
+    private func attribute() {
+        setup()
+        setupNavigationBar()
+    }
+
+    private func layout() {
+        view.addSubviews(descriptionLabel, alarmSetView, alarmAgainSetView, alarmAgainSetDescriptionLabel)
+
+        NSLayoutConstraint.activate([
+            descriptionLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            descriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+
+        NSLayoutConstraint.activate([
+            alarmSetView.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 16),
+            alarmSetView.leadingAnchor.constraint(equalTo: descriptionLabel.leadingAnchor),
+            alarmSetView.trailingAnchor.constraint(equalTo: descriptionLabel.trailingAnchor),
+            alarmSetView.heightAnchor.constraint(greaterThanOrEqualToConstant: 80)
+        ])
+
+        NSLayoutConstraint.activate([
+            alarmAgainSetView.topAnchor.constraint(equalTo: alarmSetView.bottomAnchor, constant: 16),
+            alarmAgainSetView.leadingAnchor.constraint(equalTo: descriptionLabel.leadingAnchor),
+            alarmAgainSetView.trailingAnchor.constraint(equalTo: descriptionLabel.trailingAnchor),
+            alarmAgainSetView.heightAnchor.constraint(greaterThanOrEqualToConstant: 205)
+        ])
+
+        NSLayoutConstraint.activate([
+            alarmAgainSetDescriptionLabel.topAnchor.constraint(equalTo: alarmAgainSetView.bottomAnchor, constant: 8),
+            alarmAgainSetDescriptionLabel.trailingAnchor.constraint(equalTo: alarmAgainSetView.trailingAnchor, constant: -16)
+        ])
+    }
+
+    private func setup() {
+        view.backgroundColor = .secondarySystemBackground
+    }
+    
+    private func setupNavigationBar() {
+        title = "알림"
+    }
+}
---
+++ Alarmi/Alarmi/Sources/ViewControllers/SettingAlarm/Supporting Views/AlarmAgainSetView.swift
@@ -0,0 +1,169 @@
+//
+//  AlarmAgainView.swift
+//  Alarmi
+//
+//  Created by Woody on 2022/07/16.
+//  Copyright © 2022 MoTe. All rights reserved.
+//
+
+import UIKit
+
+final class AlarmAgainSetView: UIView {
+
+    // MARK: Views
+
+    private let vStackView: UIStackView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.axis = .vertical
+        $0.spacing = 4
+        $0.distribution = .fill
+        $0.alignment = .leading
+        return $0
+    }(UIStackView())
+
+    private let hStackView1: UIStackView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.axis = .horizontal
+        $0.spacing = 4
+        $0.distribution = .fill
+        $0.alignment = .center
+        return $0
+    }(UIStackView())
+
+    private let titleLabel: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.setDynamicFont(.title2)
+        $0.text = "다시 알림"
+        return $0
+    }(UILabel())
+
+    private let descriptionLabel: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.textColor = .secondaryLabel
+        $0.setDynamicFont(.footnote)
+        $0.text = "알림이 온 이후 전화를 기록하지 않으면,\n알림을 다시 보냅니다."
+        return $0
+    }(UILabel())
+
+    private let alarmSwitch: UISwitch = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.isOn = true
+        return $0
+    }(UISwitch())
+
+    private let subTitleLabel: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.text = "횟수"
+        $0.setDynamicFont(.headline)
+        return $0
+    }(UILabel())
+
+    private let hStackView2: UIStackView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.axis = .horizontal
+        $0.spacing = 6
+        $0.distribution = .fill
+        $0.alignment = .center
+        return $0
+    }(UIStackView())
+
+    private let minLabel: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.text = "2"
+        $0.setDynamicFont(.callout)
+        $0.textColor = .secondaryLabel
+        return $0
+    }(UILabel())
+
+    private let maxLabel: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.text = "10"
+        $0.setDynamicFont(.callout)
+        $0.textColor = .secondaryLabel
+        return $0
+    }(UILabel())
+
+    private lazy var slider: UISlider = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.minimumValue = 2
+        $0.maximumValue = 10
+        $0.value = 6
+        $0.addTarget(self, action: #selector(sliderValueChanged), for: .valueChanged)
+        return $0
+    }(UISlider())
+
+    private let sliderLabel: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.text = "최대 6번 알림을 다시 받을게요."
+        $0.textAlignment = .center
+        return $0
+    }(UILabel())
+
+    // MARK: Parameters
+
+    private var alarmAgainCount: Int = 5 {
+        didSet {
+            sliderLabel.text = "최대 \(alarmAgainCount)번 알림을 다시 받을게요."
+        }
+    }
+
+    // MARK: Life Cycle
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        attribute()
+        layout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: Methods
+
+    private func attribute() {
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 15
+        layer.masksToBounds = true
+    }
+
+    private func layout() {
+        addSubviews(hStackView1, subTitleLabel, hStackView2, sliderLabel)
+
+        vStackView.addArrangedSubviews(titleLabel, descriptionLabel)
+
+        hStackView1.addArrangedSubviews(vStackView, alarmSwitch)
+
+        hStackView2.addArrangedSubviews(minLabel, slider, maxLabel)
+
+        NSLayoutConstraint.activate([
+            hStackView1.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+            hStackView1.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            hStackView1.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16)
+        ])
+
+        NSLayoutConstraint.activate([
+            subTitleLabel.topAnchor.constraint(equalTo: hStackView1.bottomAnchor, constant: 16),
+            subTitleLabel.leadingAnchor.constraint(equalTo: hStackView1.leadingAnchor)
+        ])
+
+        NSLayoutConstraint.activate([
+            hStackView2.topAnchor.constraint(equalTo: subTitleLabel.bottomAnchor, constant: 8),
+            hStackView2.leadingAnchor.constraint(equalTo: hStackView1.leadingAnchor),
+            hStackView2.trailingAnchor.constraint(equalTo: hStackView1.trailingAnchor)
+        ])
+
+        NSLayoutConstraint.activate([
+            sliderLabel.topAnchor.constraint(equalTo: hStackView2.bottomAnchor, constant: 8),
+            sliderLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16),
+            sliderLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            sliderLabel.leadingAnchor.constraint(equalTo: hStackView1.leadingAnchor),
+            sliderLabel.trailingAnchor.constraint(equalTo: hStackView1.trailingAnchor)
+        ])
+    }
+
+    @objc private func sliderValueChanged() {
+        alarmAgainCount = Int(slider.value)
+    }
+}
