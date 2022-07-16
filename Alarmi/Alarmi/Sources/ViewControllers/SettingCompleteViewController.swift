55170187bc08bfafbb59b49ae0456d364ced9e40
Chaeho-Min <james118@naver.com>
2022-07-16 12:43:33 +0900

    ✨ SettingCompleteViewController 파일 생성

---
+++ Alarmi/Alarmi/Sources/ViewControllers/SettingCompleteViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SettingCompleteViewController.swift
+//  Alarmi
+//
+//  Created by 민채호 on 2022/07/16.
+//  Copyright © 2022 MoTe. All rights reserved.
+//
+
+import UIKit
+
+class SettingCompleteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemGroupedBackground
+    }
+    
+    private func attribute() {
+        
+    }
+    
+    private func layout() {
+        
+    }
+}
