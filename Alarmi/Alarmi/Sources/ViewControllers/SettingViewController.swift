9778e3b0d55ee94a590e1f43d5873d7d2945ee9a
Chaeho-Min <james118@naver.com>
2022-07-16 20:33:40 +0900

    ✨ SettingViewController 기본 구조 구성

--- Alarmi/Alarmi/Sources/ViewControllers/SettingViewController.swift
+++ Alarmi/Alarmi/Sources/ViewControllers/SettingViewController.swift
@@ -9,8 +9,39 @@
 import UIKit
 
 final class SettingViewController: UIViewController {
+    
+    // MARK: View
+    
+    // MARK: Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        attribute()
+        layout()
+    }
+    
+    // MARK: Method
+    
+    func attribute() {
+        
+    }
+    
+    func layout() {
+        
     }
 }
+
+// MARK: - Preview
+
+#if DEBUG
+import SwiftUI
+
+struct SettingViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        SettingViewController()
+            .toPreview()
+            .ignoresSafeArea()
+    }
+}
+#endif
