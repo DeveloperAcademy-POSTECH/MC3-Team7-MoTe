9548f99bbfb739234b34cf8205adf8852850e804
Chaeho-Min <james118@naver.com>
2022-07-16 20:31:18 +0900

    ✨ SettingViewController 파일 생성

---
+++ Alarmi/Alarmi/Sources/ViewControllers/SettingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingViewController.swift
+//  Alarmi
+//
+//  Created by 민채호 on 2022/07/16.
+//  Copyright © 2022 MoTe. All rights reserved.
+//
+
+import UIKit
+
+final class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
