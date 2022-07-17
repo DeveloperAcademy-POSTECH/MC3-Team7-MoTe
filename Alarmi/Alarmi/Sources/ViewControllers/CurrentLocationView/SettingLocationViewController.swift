915f6797ddd4760f6b2a1af08c302fd35cea26db
JI SOO <dlwltn815@gmail.com>
2022-07-17 18:19:19 +0900

    ✨ SettingLocationViewController 생성

---
+++ Alarmi/Alarmi/Sources/ViewControllers/CurrentLocationView/SettingLocationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CurrentLocationViewController.swift
+//  Alarmi
+//
+//  Created by LeeJiSoo on 2022/07/16.
+//  Copyright © 2022 MoTe. All rights reserved.
+//
+
+import UIKit
+
+class SettingLocationViewController: UIViewController {
+
+}
