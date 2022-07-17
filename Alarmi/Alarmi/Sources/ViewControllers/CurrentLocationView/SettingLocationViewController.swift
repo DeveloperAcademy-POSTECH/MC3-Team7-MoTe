55f73605981976ebe420f2d68c94c4ca24fd6189
JI SOO <dlwltn815@gmail.com>
2022-07-17 18:24:56 +0900

    🎨 view 프로퍼티 추가

--- Alarmi/Alarmi/Sources/ViewControllers/CurrentLocationView/SettingLocationViewController.swift
+++ Alarmi/Alarmi/Sources/ViewControllers/CurrentLocationView/SettingLocationViewController.swift
@@ -10,4 +10,26 @@ import UIKit
 
 class SettingLocationViewController: UIViewController {
 
+    // MARK: View
+
+    private let descriptionLabel: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.text = "지금 어디에 계신가요?"
+        $0.setDynamicFont(.body)
+        return $0
+    }(UILabel())
+
+    private lazy var collectionView: UICollectionView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.delegate = self
+        $0.dataSource = self
+        $0.register(SettingLocationCollectionViewCell.self, forCellWithReuseIdentifier: "SettingLocationCollectionViewCell")
+        $0.backgroundColor = .systemGroupedBackground
+        return $0
+    }(UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()))
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
 }
