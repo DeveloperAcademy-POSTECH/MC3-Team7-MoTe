50fd2f03211de8562901dbf7917e8aeda42aea5a
JI SOO <dlwltn815@gmail.com>
2022-07-18 12:00:39 +0900

    ♻️  들어가면 안되는 코드 수정

--- Alarmi/Alarmi/Sources/SceneDelegate.swift
+++ Alarmi/Alarmi/Sources/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = scene.windows.first ?? UIWindow(frame: UIScreen.main.bounds)
-        let mainTabBarController = UINavigationController(rootViewController: SettingLocationViewController())
+        let mainTabBarController = MainTabBarController()
         window?.rootViewController = mainTabBarController
         window?.makeKeyAndVisible()
 
