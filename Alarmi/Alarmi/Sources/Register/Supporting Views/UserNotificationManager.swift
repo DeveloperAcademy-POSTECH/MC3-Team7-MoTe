feeb4bc5a258d238f2b3c7a52e06dfcbde57bd66
JI SOO <dlwltn815@gmail.com>
2022-07-25 23:17:32 +0900

    ✨ 대기중인 request 보여주는 디버깅 코드 추가 (feat. 고반)

--- Alarmi/Alarmi/Sources/Register/Supporting Views/UserNotificationManager.swift
+++ Alarmi/Alarmi/Sources/Register/Supporting Views/UserNotificationManager.swift
@@ -9,4 +9,21 @@
 import UserNotifications
 
 final class UserNotificationManager {
+    // 현재 대기중인 noti 보여줌 (고반의 코드)
+    func checkPendingNotificationRequest() {
+        notificationCenter.getPendingNotificationRequests { (notificationRequests) in
+            if notificationRequests.isEmpty {
+                print("Pending request 없음")
+            } else{
+                for notification: UNNotificationRequest in notificationRequests {
+                    print("----------requests info------------")
+                    print(notification.identifier)
+                    print(notification.content.title)
+                    print(notification.content.body)
+                    print(notification.trigger ?? "No trigger")
+                    print("-----------------------------------")
+                }
+            }
+        }
+    }
 }
