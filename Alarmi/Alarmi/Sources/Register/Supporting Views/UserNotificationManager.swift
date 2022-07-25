4375baae3bdc396fbc3a0388d7e297fee8ccb0ad
JI SOO <dlwltn815@gmail.com>
2022-07-25 23:16:40 +0900

    ✨ 알림매니저 생성

---
+++ Alarmi/Alarmi/Sources/Register/Supporting Views/UserNotificationManager.swift
@@ -0,0 +1,12 @@
+//
+//  UserNotificationManager.swift
+//  Alarmi
+//
+//  Created by LeeJiSoo on 2022/07/25.
+//  Copyright © 2022 MoTe. All rights reserved.
+//
+
+import UserNotifications
+
+final class UserNotificationManager {
+}
