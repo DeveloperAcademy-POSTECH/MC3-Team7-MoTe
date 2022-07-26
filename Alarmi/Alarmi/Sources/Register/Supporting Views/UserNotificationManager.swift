54253eb21828a8ddc187933771821894f7cc43c6
JI SOO <dlwltn815@gmail.com>
2022-07-26 22:23:30 +0900

    ✨ 알림매니저 수정 (generate, send, makeContent)

--- Alarmi/Alarmi/Sources/Register/Supporting Views/UserNotificationManager.swift
+++ Alarmi/Alarmi/Sources/Register/Supporting Views/UserNotificationManager.swift
@@ -12,55 +12,69 @@ final class UserNotificationManager {
     private let notificationCenter = UNUserNotificationCenter.current()
     // TODO: 그리즐리 유저디폴트
     private let contactStartDate = Date() // Model - Goal - startDate
-    private let notificationPeriod = 7 // Model - Goal - peroid
+    private var notificationPeriod = 7.0 // Model - Goal - peroid
     private let notificationCycle = 100
     private let startTime = Date() // date형태이기 때문에 임시로 이렇게 생성
+    private var identifier = ""
     private lazy var endTime = Calendar.current.date(byAdding: .minute, value: 60, to: startTime)
 
-    private var identifierDateFormatter: DateFormatter {
+    private var identifierFormatter: DateFormatter {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyyMMdd"
+        dateFormatter.dateFormat = "yyyyMMdd a hh:mm"
         dateFormatter.timeZone = TimeZone(abbreviation: "KST")
         return dateFormatter
     }
 
-    private var identifierTimeFormatter: DateFormatter {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "a hh:mm"
-        dateFormatter.timeZone = TimeZone(abbreviation: "KST")
-        return dateFormatter
-    }
-
-    func makeNotificationRequest() {
-        var notificationRequestDate = Calendar.current.date(byAdding: .day, value: notificationPeriod, to: contactStartDate)
-        for _ in 0..<notificationCycle {
-            for _ in 0..<2 {
-                var requestStartDateComponents = Calendar.current.dateComponents([.year, .month, .day], from: notificationRequestDate ?? Date())
-                requestStartDateComponents.hour = 14 // startTime의 hour추출해야함
-                requestStartDateComponents.minute = 0 // startTime의 min 추출해야함
-                let notificationTrigger = UNCalendarNotificationTrigger(dateMatching: requestStartDateComponents, repeats: false)
-                let notificationContent = makeNotificationContent()
-                let identifierDate = identifierDateFormatter.string(from: notificationRequestDate ?? Date())
-                let request = UNNotificationRequest(identifier: identifierDate, content: notificationContent, trigger: notificationTrigger)
-                self.notificationCenter.add(request) { error in
-                    if let error = error {
-                        print(error)
-                    }
+    // 권한요청 확인
+    func generateUserNotification() {
+        notificationCenter.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if let error = error {
+                print(error)
+            } else {
+                if granted {
+                    self.sendUserNotification()
                 }
             }
-            notificationRequestDate = Calendar.current.date(byAdding: .day, value: 1, to: notificationRequestDate ?? Date())
         }
     }
 
+    func sendUserNotification() {
+        UNUserNotificationCenter.current().getNotificationSettings { [self] (settings) in
+            guard settings.authorizationStatus == UNAuthorizationStatus.authorized else { return }
+            for type in 0..<2 {
+                if type == 0 {
+                    self.identifier = self.identifierFormatter.string(from: self.contactStartDate)
+                } else {
+                    self.identifier = self.identifierFormatter.string(from: self.endTime ?? Date())
+                }
+                let content = self.makeNotificationContent(type)
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: self.notificationPeriod, repeats: false)
+                let request = UNNotificationRequest(identifier: self.identifier, content: content, trigger: trigger)
+                UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+                self.notificationPeriod += 10.0
+            }
+        }
+    }
+
+    // TODO: 연락 커밋을 눌렀을대 해당 메서드를 불러서 request 모두 삭제
     func removeAllPendingRequest() {
         notificationCenter.removeAllPendingNotificationRequests()
         print("request 전부 삭제")
     }
 
-    func makeNotificationContent() -> UNMutableNotificationContent {
+    // TODO: 연락 문구 정해야함 -> 일단 두개
+    func makeNotificationContent(_ type: Int) -> UNMutableNotificationContent {
         let notificationContent = UNMutableNotificationContent()
-        notificationContent.title = "연락할때가 되었음"
-        notificationContent.body = "부모님이 기다리신다 얼른 연락하렴!"
+        notificationContent.title = ""
+        notificationContent.body = ""
+        switch type {
+        case 0:
+            notificationContent.title = "연락할시간이다!"
+            notificationContent.body = "연락할 시간이다!"
+        default:
+            notificationContent.title = "연락할 시간이 끝나가요!"
+            notificationContent.body = "연락할 시간이 끝나가요!"
+        }
         return notificationContent
     }
 
