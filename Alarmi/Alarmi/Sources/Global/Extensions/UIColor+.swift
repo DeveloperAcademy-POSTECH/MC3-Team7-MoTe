92da868da293cdb2cf918b5e4e9ff4562daf4d31
Chaeho-Min <james118@naver.com>
2022-07-27 20:50:14 +0900

    💄 UIColor+ 파일 추가

---
+++ Alarmi/Alarmi/Sources/Global/Extensions/UIColor+.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+.swift
+//  Alarmi
+//
+//  Created by 민채호 on 2022/07/27.
+//  Copyright © 2022 MoTe. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let backgroundColor = UIColor(named: "Background")
+    static let goalDateRed = UIColor(named: "GoalDateRed")
+    static let toggleOrange = UIColor(named: "ToggleOrange")
+}
