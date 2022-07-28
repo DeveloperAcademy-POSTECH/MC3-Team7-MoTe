1d608ee5fb6345be05e06f51ee726115605e8664
Chaeho-Min <james118@naver.com>
2022-07-28 18:37:58 +0900

    ✨ AlarmSettingView 생성

---
+++ Alarmi/Alarmi/Sources/MainTab/Setting/AlarmSettingView.swift
@@ -0,0 +1,39 @@
+//
+//  AlarmSettingView.swift
+//  Alarmi
+//
+//  Created by 민채호 on 2022/07/28.
+//  Copyright © 2022 MoTe. All rights reserved.
+//
+
+import UIKit
+
+final class AlarmSettingView: UIView {
+    
+    // MARK: Views
+    
+    // MARK: Property
+    
+    // MARK: Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        attribute()
+        layout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Methods
+    
+    private func attribute() {
+        
+    }
+    
+    private func layout() {
+        
+    }
+}
