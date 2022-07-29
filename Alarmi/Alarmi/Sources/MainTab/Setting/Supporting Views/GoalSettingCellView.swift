e71b1a929a7f2a5614812e1399ba7f430672bba2
Chaeho-Min <james118@naver.com>
2022-07-29 21:26:09 +0900

    ✨ GoalSettingCellView 뷰 추가

--- Alarmi/Alarmi/Sources/MainTab/Setting/Supporting Views/GoalSettingCellView.swift
+++ Alarmi/Alarmi/Sources/MainTab/Setting/Supporting Views/GoalSettingCellView.swift
@@ -12,6 +12,30 @@ final class GoalSettingCellView: UIView {
     
     // MARK: Views
     
+    private let goalPeriodLabel: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.setDynamicFont(.body)
+        $0.text = "7"
+        return $0
+    }(UILabel())
+    
+    private let goalPeriodDescriptionLabel: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.setDynamicFont(.body)
+        $0.textColor = .secondaryLabel
+        $0.text = "일에 한 번"
+        return $0
+    }(UILabel())
+    
+    private let goalPeriodStepper: UIStepper = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.value = 7
+        $0.minimumValue = 1
+        $0.maximumValue = 31
+        $0.stepValue = 1
+        return $0
+    }(UIStepper())
+    
     // MARK: Property
     
     // MARK: Life Cycle
@@ -30,7 +54,9 @@ final class GoalSettingCellView: UIView {
     // MARK: Methods
     
     private func attribute() {
-        
+        backgroundColor = .secondarySystemGroupedBackground
+        layer.cornerRadius = 10
+        layer.masksToBounds = true
     }
     
     private func layout() {
