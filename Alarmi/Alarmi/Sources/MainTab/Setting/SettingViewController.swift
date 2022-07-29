ce3561f97cf84bdf32d3ad26835c267d6783e89e
Chaeho-Min <james118@naver.com>
2022-07-29 17:41:39 +0900

    ✨ SettingViewController에 뷰 추가

--- Alarmi/Alarmi/Sources/MainTab/Setting/SettingViewController.swift
+++ Alarmi/Alarmi/Sources/MainTab/Setting/SettingViewController.swift
@@ -13,6 +13,36 @@ final class SettingViewController: UIViewController {
 
     // MARK: View
     
+    private let callTimeSettingVStack: UIStackView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.axis = .vertical
+        $0.spacing = 8
+        $0.distribution = .fill
+        $0.alignment = .leading
+        return $0
+    }(UIStackView())
+    
+    private let callTimeSettingTitleLabel: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.setDynamicFont(for: .title3, weight: .bold)
+        $0.text = "전화 시간 설정"
+        return $0
+    }(UILabel())
+    
+    private lazy var callTimeCellView: CallTimeCellView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(CallTimeCellView())
+    
+    private let callTimeSettingDescriptionLabel: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.setDynamicFont(.caption1)
+        $0.textAlignment = .right
+        $0.textColor = .secondaryLabel
+        $0.text = "알림을 설정하면 목표일의 이 시간대에 알림을 보내드려요."
+        return $0
+    }(UILabel())
+    
     private let alarmSettingVStack: UIStackView = {
         $0.translatesAutoresizingMaskIntoConstraints = false
         $0.axis = .vertical
@@ -39,7 +69,7 @@ final class SettingViewController: UIViewController {
         $0.setDynamicFont(.caption1)
         $0.textAlignment = .right
         $0.textColor = .secondaryLabel
-        $0.text = "알림이 온 이후에 전화를 기록하지 않으면,\n전화 시간 종료 전에 알림을 다시 보내드려요."
+        $0.text = "알림이 온 이후에 전화를 기록하지 않으면,\n전화 시간 종료 전에 알림을 총 6번 보내드려요."
         return $0
     }(UILabel())
 
