a46aea999233bced7ffec527599725a20c73f8a8
Chaeho-Min <james118@naver.com>
2022-07-31 23:44:53 +0900

    ✨ RegisterCompleteViewController에서 알림 설정 기본값 주도록 변경

--- Alarmi/Alarmi/Sources/Register/RegisterCompleteViewController.swift
+++ Alarmi/Alarmi/Sources/Register/RegisterCompleteViewController.swift
@@ -58,6 +58,7 @@ final class RegisterCompleteViewController: UIViewController {
 
     weak var delegate: RegisterCompleteViewControllerDelegate?
     private let userNotificationManager = UserNotificationManager()
+    private let alarmUserDefaults = AlarmUserefaults(key: .alarm)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -99,6 +100,7 @@ final class RegisterCompleteViewController: UIViewController {
     @objc private func buttonDidTap() {
         userNotificationManager.requestAuthorization {
             DispatchQueue.main.async {
+                self.alarmUserDefaults.save(Alarm(isAlarm: true, isAlarmAgain: true))
                 self.delegate?.finishRegister()
             }
         }
