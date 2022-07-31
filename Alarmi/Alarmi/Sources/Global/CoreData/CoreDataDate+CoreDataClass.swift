f6a636efcc92fffa1107146efeb914449d7cb96b
Grizzly <dydrhks02@naver.com>
2022-07-31 20:46:44 +0900

    ✨ 코어 데이터 새로 생성

---
+++ Alarmi/Alarmi/Sources/Global/CoreData/CoreDataDate+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  CoreDataDate+CoreDataClass.swift
+//  Alarmi
+//
+//  Created by 임 용관 on 2022/07/31.
+//  Copyright © 2022 MoTe. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CoreDataDate)
+public class CoreDataDate: NSManagedObject {
+
+}
